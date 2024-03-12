6d4e11d15708cd3e8b700558abf00ce17beb9586
rossushii <raspadernilla@gmai.com>
2024-03-12 21:45:38 +0800

    Moving Zeros

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.moveZeros = void 0;
+const moveZeros = (arr) => {
+    let nonZeroIndex = 0;
+    // Iterate through the array and move non-zero elements to the front
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            arr[nonZeroIndex] = arr[i];
+            nonZeroIndex++;
+        }
+    }
+    // Fill the remaining elements with zeros
+    for (let i = nonZeroIndex; i < arr.length; i++) {
+        arr[i] = 0;
+    }
+    return arr;
+};
+exports.moveZeros = moveZeros;
+const exampleArray = [false, 1, 0, 1, 2, 0, 1, 3, "a"];
+const modifiedArray = (0, exports.moveZeros)(exampleArray);
+const resultArray = JSON.stringify(modifiedArray);
+console.log(resultArray);
